595adf76d9c9f51c76c35b95c4d347a6d05d4b3c
Magno Bustamante <magpex13@github.com>
2017-06-22 21:55:00 -0500

    MultiThreading Clients

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 
 namespace ChatServer
 {
@@ -6,7 +10,38 @@ namespace ChatServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int port = 1234;
+            TcpListener tcpServer = new TcpListener(IPAddress.Any,port);
+            tcpServer.Start();
+            TcpClient tcpClient;
+            while(true)
+            {
+                Console.WriteLine("Listening...");
+                tcpClient = tcpServer.AcceptTcpClientAsync().Result;
+                new Thread(() => Program.connection(tcpClient)).Start();
+            }
+        }
+        
+        static void connection(TcpClient tcpClient)
+        {
+            using(NetworkStream stream = tcpClient.GetStream())
+            {
+                while(true)
+                {
+                    if(stream.DataAvailable)
+                    {
+                        Console.WriteLine("Received");
+                        byte[] bytesReceived = new byte[256];
+                        stream.Read(bytesReceived,0,bytesReceived.Length);
+                        string messageReceived = Encoding.ASCII.GetString(bytesReceived);
+                        Console.WriteLine("Message Received : " + messageReceived.Substring(0,messageReceived.IndexOf('\0'))); //Al ser el buffer de tamaño 256, se obtiene la cadena mandada por el cliente (Que normalmente, su tamaño es menor a 256) y el resto basura.
+                        byte[] bytesSend = Encoding.ASCII.GetBytes(messageReceived);
+                        stream.Write(bytesSend,0,bytesSend.Length);
+                    }
+                }
+            }
+
+            //tcpClient.Dispose();
         }
     }
 }
